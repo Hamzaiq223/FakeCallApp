413cff683e4ad582faf82c253629332cd6206f74
mac <mac@macs-mbp-2.home>
2024-03-14 23:52:53 +0500

    Audio Played Successfully

--- app/src/main/java/com/mai/fake/prank/call/audio/call/app/recorder/Activities/AudioCall/AudioCall.kt
+++ app/src/main/java/com/mai/fake/prank/call/audio/call/app/recorder/Activities/AudioCall/AudioCall.kt
@@ -3,116 +3,120 @@ package com.mai.fake.prank.call.audio.call.app.recorder.Activities.AudioCall
 import android.content.Context
 import android.content.SharedPreferences
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.mai.fake.prank.call.audio.call.app.recorder.R
+import com.mai.fake.prank.call.audio.call.app.recorder.databinding.ActivityAudioCallBinding
+
+import java.io.IOException
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AudioCall : AppCompatActivity() {
-
-    private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var handler: Handler
-    private lateinit var audioFiles: MutableList<String>
-    private var currentAudioIndex: Int = 0
-    private lateinit var sharedPreferences: SharedPreferences
-    private var isPaused: Boolean = false
+    private var mediaPlayer: MediaPlayer? = null
+    private var isTimerRunning = false
+    private var startTimeInMillis: Long = 0
+    private var timer: CountDownTimer? = null
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageRef: StorageReference
+    private lateinit var binding: ActivityAudioCallBinding
+    private var isAudioPlaying = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_audio_call)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_audio_call)
 
-        handler = Handler()
+        storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
 
-        audioFiles = mutableListOf()
+        playAudioWithTimer()
+    }
 
-        // Initialize SharedPreferences
-        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+    private fun playAudioWithTimer() {
+        val folderName = "Ronaldo"
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        var lastPlayedAudioIndex = sharedPreferences.getInt("lastPlayedAudioIndex", -1)
 
-        // Start playing audio
-        playAudio()
+        val folderRef = storageRef.child(folderName)
+
+        folderRef.listAll()
+            .addOnSuccessListener { listResult ->
+                val audioRefs = listResult.items as ArrayList<StorageReference>
+                if (audioRefs.isNotEmpty() && !isAudioPlaying) {
+                    val nextAudioIndex = (lastPlayedAudioIndex + 1) % audioRefs.size
+                    val nextAudioRef = audioRefs[nextAudioIndex]
+
+                    nextAudioRef.downloadUrl
+                        .addOnSuccessListener { uri ->
+                            val audioUrl = uri.toString()
+                            if (mediaPlayer == null) {
+                                mediaPlayer = MediaPlayer()
+                            }
+                            try {
+                                mediaPlayer!!.reset()
+                                mediaPlayer!!.setDataSource(audioUrl)
+                                mediaPlayer!!.prepare()
+                                mediaPlayer!!.start()
+                                startTimeInMillis = System.currentTimeMillis()
+                                startTimer()
+                                val editor = sharedPreferences.edit()
+                                editor.putInt("lastPlayedAudioIndex", nextAudioIndex)
+                                editor.apply()
+                                isAudioPlaying = true
+                            } catch (e: IOException) {
+                                e.printStackTrace()
+                            }
+                        }
+                        .addOnFailureListener {
+                            // Handle error
+                        }
+                }
+            }
+            .addOnFailureListener {
+                // Handle error
+            }
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(Long.MAX_VALUE, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val elapsedTimeInMillis = System.currentTimeMillis() - startTimeInMillis
+                updateTimerDisplay(elapsedTimeInMillis)
+            }
+
+            override fun onFinish() {}
+        }
+        timer!!.start()
+        isTimerRunning = true
+    }
+
+    private fun updateTimerDisplay(elapsedTimeInMillis: Long) {
+        val minutes = (elapsedTimeInMillis / 1000 / 60).toInt()
+        val seconds = (elapsedTimeInMillis / 1000 % 60).toInt()
+        val timeString = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+        binding.tvTimer.text = timeString
     }
 
     override fun onResume() {
         super.onResume()
-        if (isPaused) {
-            isPaused = false
-            playAudio()
-        }
-    }
-
-    private fun playAudio() {
-        mediaPlayer = MediaPlayer()
-        val folderName = "Ronaldo"
-        val storage = FirebaseStorage.getInstance()
-        val storageRef = storage.reference.child(folderName)
-
-        val lastPlayedAudioIndex = sharedPreferences.getInt("lastPlayedAudioIndex", -1)
-
-        storageRef.listAll().addOnSuccessListener { listResult ->
-            audioFiles.addAll(listResult.items.map { it.name })
-
-            if (audioFiles.isNotEmpty()) {
-                currentAudioIndex = if (lastPlayedAudioIndex != -1 && lastPlayedAudioIndex < audioFiles.size) {
-                    lastPlayedAudioIndex
-                } else {
-                    0
-                }
-
-                val nextAudioRef = storageRef.child(audioFiles[currentAudioIndex])
-
-                nextAudioRef.downloadUrl.addOnSuccessListener { uri ->
-                    mediaPlayer.apply {
-                        if (isPlaying) {
-                            stop()
-                            reset()
-                        }
-                        setDataSource(this@AudioCall, Uri.parse(uri.toString()))
-                        prepare()
-                        start()
-                    }
-
-                    // Start timer
-                    handler.postDelayed(updateTimer, 1000)
-
-                    // Set on completion listener
-                    mediaPlayer.setOnCompletionListener {
-                        // Move to the next audio file
-                        currentAudioIndex = (currentAudioIndex + 1) % audioFiles.size
-                        val editor = sharedPreferences.edit()
-                        editor.putInt("lastPlayedAudioIndex", currentAudioIndex)
-                        editor.apply()
-                        playAudio()
-                    }
-                }.addOnFailureListener {
-                    // Handle error
-                }
-            }
-        }.addOnFailureListener {
-            // Handle error
-        }
-    }
-
-
-    private val updateTimer = object : Runnable {
-        override fun run() {
-            // Update your timer display here
-            handler.postDelayed(this, 1000)
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mediaPlayer.release()
-        handler.removeCallbacks(updateTimer)
+        playAudioWithTimer()
     }
 
     override fun onPause() {
         super.onPause()
-        if (mediaPlayer.isPlaying) {
-            mediaPlayer.pause()
-            isPaused = true
+        if (mediaPlayer != null && mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.stop()
+            mediaPlayer!!.release()
+            mediaPlayer = null
         }
+        if (timer != null) {
+            timer!!.cancel()
+            isTimerRunning = false
+        }
+        isAudioPlaying = false
     }
 }
