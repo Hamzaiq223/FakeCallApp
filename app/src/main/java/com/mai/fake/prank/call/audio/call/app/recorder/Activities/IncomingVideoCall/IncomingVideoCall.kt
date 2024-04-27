ca74f02ee581faedb72da60e9fb54552b445707d
mac <mac@macs-mbp-2.home>
2024-04-28 01:04:40 +0500

    Issues Fixed

--- app/src/main/java/com/mai/fake/prank/call/audio/call/app/recorder/Activities/IncomingVideoCall/IncomingVideoCall.kt
+++ app/src/main/java/com/mai/fake/prank/call/audio/call/app/recorder/Activities/IncomingVideoCall/IncomingVideoCall.kt
@@ -2,6 +2,7 @@ package com.mai.fake.prank.call.audio.call.app.recorder.Activities.IncomingVideo
 
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -11,13 +12,17 @@ import android.os.*
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.mai.fake.prank.call.audio.call.app.recorder.Activities.VideoCall.VideoCalling
 import com.mai.fake.prank.call.audio.call.app.recorder.Common.BlurBuilder
 import com.mai.fake.prank.call.audio.call.app.recorder.Common.CharacterImageHelper
+import com.mai.fake.prank.call.audio.call.app.recorder.Common.FlashlightController
 import com.mai.fake.prank.call.audio.call.app.recorder.R
-
+import com.ncorti.slidetoact.SlideToActView
 
 
 class IncomingVideoCall : AppCompatActivity() {
@@ -26,11 +31,16 @@ class IncomingVideoCall : AppCompatActivity() {
     private lateinit var storageRef: StorageReference
     private lateinit var ivUser: ImageView
     private lateinit var ivBlurBackground: ImageView
+    private lateinit var btnSlide: SlideToActView
+    private lateinit var layoutMessage: LinearLayout
+    private lateinit var btnReturn: TextView
+    private lateinit var layoutReminder: LinearLayout
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var vibrator: Vibrator
     private val handler = Handler()
     private var isVibrating = false
     private var receivedString: String? = null
+    private val buttonHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +56,10 @@ class IncomingVideoCall : AppCompatActivity() {
 
         ivUser = findViewById(R.id.ivUser)
         ivBlurBackground = findViewById(R.id.ivImage)
+        layoutMessage = findViewById(R.id.layoutMessage)
+        layoutReminder = findViewById(R.id.layoutReminder)
+        btnSlide = findViewById(R.id.btnSlide);
+        btnReturn = findViewById(R.id.btnReturn);
         storage = FirebaseStorage.getInstance()
         storageRef = storage.reference
 
@@ -57,6 +71,21 @@ class IncomingVideoCall : AppCompatActivity() {
         val originalBitmap: Bitmap =
             characterDrawable?.let { BitmapFactory.decodeResource(resources, it) }!!
         val blurredBitmap = BlurBuilder.blur(this, originalBitmap)
+
+        btnSlide.onSlideCompleteListener = object : SlideToActView.OnSlideCompleteListener {
+            override fun onSlideComplete(view: SlideToActView) {
+                val intent = Intent(this@IncomingVideoCall, VideoCalling::class.java)
+                intent.putExtra("characterName", receivedString)
+                startActivity(intent)
+                remove()
+                showButton()
+            }
+        }
+
+
+        btnReturn.setOnClickListener{
+            finish()
+        }
 
         ivBlurBackground.setImageBitmap(blurredBitmap)
         ivUser.setImageBitmap(originalBitmap)
@@ -71,6 +100,7 @@ class IncomingVideoCall : AppCompatActivity() {
                 // Stop the vibration when the ringtone ends
                 stopVibration()
                 handler.removeCallbacks(vibrationRunnable)
+
             }
         }
 
@@ -78,7 +108,16 @@ class IncomingVideoCall : AppCompatActivity() {
         handler.post(vibrationRunnable)
     }
 
-
+    private fun showButton(){
+        val delayMillis = 3000 // Delay in milliseconds (2 seconds in this example)
+        buttonHandler.postDelayed({
+            // Code block to be executed after the delay
+            btnSlide.visibility = View.GONE
+            layoutMessage.visibility = View.GONE
+            layoutReminder.visibility = View.GONE
+            btnReturn.visibility = View.VISIBLE
+        }, delayMillis.toLong())
+    }
 
     // Method to start vibrating the device
     private fun startVibration() {
@@ -90,11 +129,17 @@ class IncomingVideoCall : AppCompatActivity() {
                     vibrate(VibrationEffect.createWaveform(longArrayOf(0, 500), 0))
                 }
             }
+
+            val flashlightController = FlashlightController(this)
+            // To turn on the flashlight
+            flashlightController.turnOnFlash()
+            // To turn off the flashlight
+            flashlightController.turnOffFlash()
+
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
-
 
     // Method to stop vibrating the device
     private fun stopVibration() {
@@ -107,10 +152,14 @@ class IncomingVideoCall : AppCompatActivity() {
 
     private val vibrationRunnable: Runnable = object : Runnable {
         override fun run() {
+
             if (isVibrating) {
                 stopVibration()
+                // To turn on the flashlight
             } else {
                 startVibration()
+                // To turn off the flashlight
+
             }
             // Toggle the vibration state
             isVibrating = !isVibrating
@@ -119,14 +168,19 @@ class IncomingVideoCall : AppCompatActivity() {
         }
     }
 
-
-    override fun onDestroy() {
-        super.onDestroy()
+    private fun remove(){
+        handler.removeCallbacksAndMessages(null) // Cancel all scheduled callbacks
         stopVibration()
+
         // Release the MediaPlayer and stop the vibration when the activity is destroyed
         mediaPlayer?.let {
             it.release()
         }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        remove()
+        buttonHandler.removeCallbacksAndMessages(null)
     }
 }
--- app/src/main/java/com/mai/fake/prank/call/audio/call/app/recorder/Activities/VideoCall/VideoCalling.kt
+++ app/src/main/java/com/mai/fake/prank/call/audio/call/app/recorder/Activities/VideoCall/VideoCalling.kt
@@ -2,8 +2,10 @@ package com.mai.fake.prank.call.audio.call.app.recorder.Activities.VideoCall
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.*
 import android.media.AudioManager
@@ -14,12 +16,15 @@ import android.util.Log
 import android.view.Surface
 import android.view.TextureView
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.mai.fake.prank.call.audio.call.app.recorder.Activities.IncomingVideoCall.IncomingVideoCall
+import com.mai.fake.prank.call.audio.call.app.recorder.Activities.VideoCharacters.VideoCharacters
 import com.mai.fake.prank.call.audio.call.app.recorder.R
 import com.mai.fake.prank.call.audio.call.app.recorder.databinding.ActivityVideoCallingBinding
 
@@ -68,7 +73,11 @@ import java.util.*
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         videoCallBinding = DataBindingUtil.setContentView(this, R.layout.activity_video_calling)
 
@@ -83,6 +92,14 @@ import java.util.*
         receivedString?.let { playVideo(it) }
 
         videoCallBinding.btnVolume.setOnClickListener { toggleMute() }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
 
         playVideoForAMinute()
 
